da7de1c4359f149233f1afa283b6af402e185a7b
Ben Kolb <ben@bolbken.dev>
2020-12-22 17:24:31 -0500

    testing codebuild locally

---
+++ utils/branch-to-stage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BRANCH=`git branch --show-current`
+GIT_STAGE=`cat $1 | json ".git_branch_stage.$BRANCH"`
+STAGE=''
+
+if [ -z "$GIT_STAGE" ] 
+    then
+
+    # No corresponding stage for this git branch... return dev-<branch name>
+    STAGE="$BRANCH"
+
+else
+
+    STAGE=$GIT_STAGE
+
+fi
+
+echo $STAGE
