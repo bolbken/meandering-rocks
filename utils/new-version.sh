37e4c9a67770cef4cabbd96faa9de3e7b59b1f83
Ben Kolb <ben@bolbken.dev>
2021-01-06 23:17:49 -0500

    ci(codebuild): removed git push for lerna new version

--- utils/new-version.sh
+++ utils/new-version.sh
@@ -9,7 +9,6 @@ if [ $GIT_BRANCH = $VERSION_BRANCH ]; then
     git config --global user.email $MEANDERING_ROCKS_CI_GIT_USER_EMAIL
     git config --global user.name $MEANDERING_ROCKS_CI_GIT_USER_NAME
     yarn run new-version
-    git push
 else
     echo "Skipping versioning for stage: $STAGE"
 fi
