ab9bd835ff2b8d0389a79fa43ac67e77e8ba525e
Ben Kolb <ben@bolbken.dev>
2021-01-06 21:13:13 -0500

    ci(codebuild): fix echo quote for git creds

--- utils/new-version.sh
+++ utils/new-version.sh
@@ -5,7 +5,7 @@ VERSION_BRANCH="review"
 if [ $GIT_BRANCH = $VERSION_BRANCH ]; then
     git reset --hard HEAD
     git config --global credential.helper "store --file $HOME/.git-credentials"
-    echo -n "https://$GH_TOKEN:x-oauth-basic@github.com >> $HOME/.git-credentials"
+    echo -n "https://$GH_TOKEN:x-oauth-basic@github.com" >> $HOME/.git-credentials
     git config --global user.email $MEANDERING_ROCKS_CI_GIT_USER_EMAIL
     git config --global user.name $MEANDERING_ROCKS_CI_GIT_USER_NAME
     yarn run new-version
