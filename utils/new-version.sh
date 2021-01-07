7b21554844d04bb65dca3712cd6edcbb0c8e83ce
Ben Kolb <ben@bolbken.dev>
2021-01-06 19:08:55 -0500

    ci(codebuild): add git user name and email before lerna version

--- utils/new-version.sh
+++ utils/new-version.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-if [ $STAGE = "review" ]; then
+
+VERSION_BRANCH="review"
+
+if [ $GIT_BRANCH = $VERSION_BRANCH ]; then
     git reset --hard HEAD
+    git config --global user.email $MEANDERING_ROCKS_CI_GIT_USER_EMAIL && git config --global user.name $MEANDERING_ROCKS_CI_GIT_USER_NAME
     yarn run new-version
 else
     echo "Skipping versioning for stage: $STAGE"
